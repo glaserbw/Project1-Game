f9e352a44b490d5a8bead284ac5759a0ca1a53fa
Brian Glaser <brianglaser@Brians-MacBook-Pro.local>
2018-08-08 17:27:40 -0700

    added collision logic that works, need to get art, more enemies and timing/win events

--- js/script.js
+++ js/script.js
@@ -2,22 +2,28 @@ console.log("JS working");
 // GLOBAL VARIABLES
 var canvas;
 var canvasContext;
-var ballX = 50; 
-var ballY = 50;
-// var ballSpeedX = 10;
-var enemy1SpeedY = 15; 
+
+var enemy1SpeedY = 5;
+var enemy2SpeedY = 2;  
 
 var player1X = 275; // this is the players starting x coordinates. used in: drawEverything
 var player1Y = 575; // this is the players starting y coordinates. used in: drawEverything
 
-var enemy1X = 275 // this is the enemys starting x coordinates. used in: drawEverything - this will be CHANGED
-var enemy1Y = 50 // this is the enemys starting x coordinates. used in: drawEverything
+var playerScore = 0; // this will display the current score (displayed as a time)
+
+var enemy1X = 275; // this is the enemy 1 starting x coordinates. used in: drawEverything - this will be CHANGED
+var enemy1Y = -300; // this is the enemy 1 starting x coordinates. used in: drawEverything
+
+var enemy2X = 475; // this is the enemy 2 starting x coordinates. used in: drawEverything - this will be CHANGED
+var enemy2Y = -300; // this is the enemy 2 starting x coordinates. used in: drawEverything
 
 const PLAYER_WIDTH = 50; // this is the players width size - only adjust when adding art
 const PLAYER_HEIGHT = 100; // this is the players height size - only adjust when adding art
 
 const ENEMY_1_WIDTH = 50; // this is the enemy's width size - only adjust when adding art
 const ENEMY_1_HEIGHT = 100; // this is the enemy's height size - only adjust when adding art
+
+var time = 0; 
 
 
 // MOUSE POSITION CALCULATION - I don't know how it works but it'll spit out x , y coordinates of the mouse 
@@ -53,29 +59,63 @@ window.onload = function() {
 };
 
 function enemyReset(){
-	enemy1Y = -canvas.height;
+	//enemy 1 respawn logic
+	enemy1Y = -300;
 	enemy1X = getRandomPos();
+	enemy1SpeedY++;
+
+	// enemy 2 respawn logic
+	// enemy2Y = -enemy2Y;
+	// enemy2X = getRandomPos();
+
 }; 
 
 // End game state - not used yet 
 function endGame(){ 
-	console.log("You'll never make it home");
+	console.log("CRASH!!!");
 };
 
 // This function will move enemies downward along Y axis 
 function moveEverything(){
-	enemy1Y = enemy1Y + enemy1SpeedY;
+	enemy1Y += enemy1SpeedY;
 
 	if(enemy1Y >= canvas.height) {
 		enemyReset(); 
 	};
+
+	// enemy 2 spawn doesn't work properly because the Y position value is colliding with Enemy1 state
+	// enemy2Y += enemy2SpeedY;
+
+	// if(enemy2Y >= canvas.height) {
+	// 	enemyReset();
+	// };
 	
-	// collision detection between player position and enemy pos 
-	var playerPos = player1X && player1Y; 
-	var enemyPos = enemy1Y && enemy1X; 
-	if(playerPos = enemy1Y) {
-		endGame(); 
-	}; 
+// collision detection between player position and enemy position - Head on only 
+	// player coordinate variables
+		var Px1 = player1X;
+		var Px2 = player1X + PLAYER_WIDTH;
+
+		var Py1 = player1Y;
+		var Py2 = player1Y + PLAYER_HEIGHT; 
+
+	//enemy coordinates variables
+		var Ex1 = enemy1X;
+		var Ex2 = enemy1X + ENEMY_1_WIDTH; 
+
+		var Ey1 = enemy1Y;
+		var Ey2 = enemy1Y + ENEMY_1_HEIGHT;
+
+	// collision logic
+
+	if (((Px1 < Ex1 && Px2 > Ex1) || (Px1 < Ex2 && Ex2 < Px2)) &&
+		((Py1 < Ey1 && Py2 > Ey1) || (Py1 < Ey2 && Ey2 < Py2))
+		) {
+		endGame();
+	}
+	//logic for edge cases - not implmented but would be nice to have 
+	//(A && B) || 
+	//(A && (Py1 == Ey1 || Py2==Ey2)) || 
+	//(B && (Px1 == Ex1 || Px2==Ex2))  
 };
 
 //This function gets a random number which I can use to populate X or Y axis reset value 
@@ -83,14 +123,19 @@ function getRandomPos(min, max) {
   min = Math.ceil(canvas.width);
   max = Math.floor(0);
   return Math.floor(Math.random() * (max - min)) + min; 
-}
+};
 
 
 // Following two functions paint the rectangle boxes on the screen, including the background - art MIGHT go here 
 function drawEverything() {
 	colorRect(0,0,canvas.width,canvas.height,"black"); // background screen size and color
 	colorRect(player1X,player1Y,PLAYER_WIDTH,PLAYER_HEIGHT,"green"); // player
-	colorRect(enemy1X,enemy1Y,ENEMY_1_WIDTH,ENEMY_1_HEIGHT,"red"); // enemy 
+	colorRect(enemy1X,enemy1Y,ENEMY_1_WIDTH,ENEMY_1_HEIGHT,"red"); // enemy 1
+	// colorRect(enemy2X,enemy2Y,ENEMY_1_WIDTH,ENEMY_1_HEIGHT,"blue"); // enemy 2
+	
+	canvasContext.font="26px Helvetica";
+	canvasContext.fillStyle="white";
+	canvasContext.fillText("Time: ", 25, 50); 
 };
 
 function colorRect(leftX, topY, width, height, drawColor){ //refactors draw elements so they're easier to scale. Your player images will probably go here
@@ -105,113 +150,16 @@ function colorRect(leftX, topY, width, height, drawColor){ //refactors draw elem
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	//Old COLLIDE Logic - still works 
+	// if((Math.round(enemy1X+ENEMY_1_WIDTH,0) >= Math.round(player1X,0) && 
+	// 	Math.round(enemy1X,0) <= Math.round(player1X+PLAYER_WIDTH,0)) || (Math.round(enemy1X,0) <= Math.round(player1X+PLAYER_WIDTH,0) &&
+	// 	 Math.round(enemy1X+ENEMY_1_WIDTH,0) >= Math.round(player1X,0))) {
+		
+	// 	if (Math.round(enemy1Y+ENEMY_1_HEIGHT,0) <= Math.round(player1Y+1,0) &&
+	// 	 Math.round(enemy1Y+ENEMY_1_HEIGHT,0) >= Math.round(player1Y-1,0)) {
+	// 		endGame();
+	// 	}
+	// }
 
 
 
